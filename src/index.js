06a4c6eb72f7eb3a2e6e6ab5188c4c2fd67a125a
Artur Kuznetsov <artkuznetsov191@gmail.com>
2019-03-11 00:19:06 +0200

    feat: add func to current money

--- src/index.js
+++ src/index.js
@@ -1,5 +1,65 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let money = {};
+
+    // let half = currency % 50;
+        if(currency <= 0){
+            money = {};
+        } 
+        else if(currency >= 10000){
+            money = { error: "You are rich, my friend! We don't have so much coins for exchange"};
+        } 
+
+        else{
+
+            let resH = Math.floor(currency / 50);
+            let modH = (currency % 50);
+            if (resH > 0) {
+                money.H = resH;                
+            };
+            
+            let resQ = Math.floor(modH / 25);
+            let  modQ = (modH % 25);
+            if( resQ > 0) {
+                money.Q = resQ;               
+            };
+
+            let resD = Math.floor(modQ / 10);
+            let modD =(modQ % 10);
+            if( resD > 0){
+                money.D = resD;                
+            };
+
+            let resN = Math.floor(modD / 5);
+            let modN = (modD % 5);
+            if( resN > 0){
+                money.N = resN;                
+            };
+
+            let resP = Math.floor(modN / 1);            
+            if( resP > 0){
+                money.P = resP;                
+            };
+            //   money.D = Math.floor(modQ / 10);
+            //   let modD = (modQ % 10);
+              
+            //   money.N = Math.floor(modD / 5);
+            //   let modN = (modD % 5);
+                        
+            //   money.P = Math.floor(modN / 1);
+          
+        //   money.Q = Math.floor(modH / 25);
+        //   let modQ = (modH % 25);
+          
+        //   money.D = Math.floor(modQ / 10);
+        //   let modD = (modQ % 10);
+          
+        //   money.N = Math.floor(modD / 5);
+        //   let modN = (modD % 5);
+                    
+        //   money.P = Math.floor(modN / 1);
+        //   let modP = (modN % 1);
+        }
+        return money; 
+
 }
